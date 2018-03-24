a7ed441a9a903d04005003884b0e91d1e3402d60
podhmo <ababjam61@gmail.com>
2018-03-24 12:58:31 +0900

    Show icon (#5)
    * for loop
    
    * show-icon

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"syscall"
+
+	"os/signal"
+	"os/user"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/gen2brain/beeep"
+	"github.com/podhmo/egoist/icondownload"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type opt struct {
-	path string
+	path     string
+	showIcon bool
 }
 
 // config :
@@ -26,18 +33,64 @@ type config struct {
 func main() {
 	var opt opt
 	app := kingpin.New("egoist", "twitter client")
-	app.Flag("--config", "config file path").Short('c').Required().ExistingFileVar(&opt.path)
+	app.Flag("config", "config file path").Short('c').Required().ExistingFileVar(&opt.path)
+	app.Flag("show-icon", "show profile icon").BoolVar(&opt.showIcon)
 
 	if _, err := app.Parse(os.Args[1:]); err != nil {
 		app.FatalUsage(err.Error())
 	}
 
-	if err := run(opt.path); err != nil {
+	var downloader *icondownload.Downloader
+	if opt.showIcon {
+		u, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Join(u.HomeDir, ".config/egoist")
+		downloader = icondownload.New(filepath.Join(dir, "img"))
+		if f, _ := os.Open(filepath.Join(dir, "mapping.json")); f != nil {
+			decoder := json.NewDecoder(f)
+			var data map[int64]string
+			if err := decoder.Decode(&data); err != nil {
+				log.Println(err)
+			}
+			f.Close()
+			log.Println("load", len(data), "items")
+			downloader.Load(data)
+		}
+		downloader.Start(4)
+
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		go func() {
+			select {
+			case err := <-downloader.Error():
+				log.Println(err)
+			case <-ch:
+				<-downloader.Stop()
+				data := downloader.Data()
+				log.Println("save", len(data), "items")
+				f, err := os.Create(filepath.Join(dir, "mapping.json"))
+				if err != nil {
+					log.Println(err)
+					os.Exit(1)
+				}
+				encoder := json.NewEncoder(f)
+				if err := encoder.Encode(&data); err != nil {
+					log.Println(err)
+				}
+				f.Close()
+				os.Exit(0)
+			}
+		}()
+	}
+
+	if err := run(opt.path, downloader); err != nil {
 		log.Fatalf("%+v", err)
 	}
 }
 
-func run(path string) error {
+func run(path string, downloader *icondownload.Downloader) error {
 	var c config
 	f, err := os.Open(path)
 	if err != nil {
@@ -55,12 +108,17 @@ func run(path string) error {
 		c.ConsumerSecret,
 	)
 
-	twitterStream := api.UserStream(nil)
-	for {
-		x := <-twitterStream.C
+	for x := range api.UserStream(nil).C {
 		switch tweet := x.(type) {
 		case anaconda.Tweet:
-			if err := beeep.Notify(tweet.User.Name, tweet.FullText, "assets/information.png"); err != nil {
+			imgpath := "assets/information.png"
+			if downloader != nil {
+				if path, ok := downloader.Download(&tweet.User); ok {
+					imgpath = path
+				}
+			}
+
+			if err := beeep.Notify(tweet.User.Name, tweet.FullText, imgpath); err != nil {
 				log.Printf("%+v", err)
 			}
 		case anaconda.StatusDeletionNotice:
@@ -73,4 +131,5 @@ func run(path string) error {
 			log.Println(msg)
 		}
 	}
+	return nil
 }
